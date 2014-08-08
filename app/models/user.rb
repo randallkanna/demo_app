0beba4858adac13d9183ad9fb83fb906f0f85b1d
Randall Kanna <randallkanna@gmail.com>
2014-08-08 12:09:35 -0700

    minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :profile_name, presence: true,
-  						uniqueness: true,
-  						format: {
-  							with: /a-zA-Z0-9_-/,
-  							message: 'Must be formatted correctly'
-  						}
+                           uniqueness: true, 
+                           format: {
+                            with: /\A[a-zA-Z0-9_-]+\Z/,
+                            message: 'Must be formatted correctly.'
+                           }
 
   has_many :statuses
 
